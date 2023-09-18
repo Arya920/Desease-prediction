6acea3d7931e2d258f009455782d415602b899a6
Arya Chakraborty <chakrabortyarya97@gmail.com>
2023-09-19 02:12:40 +0530

    code Compleate

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ loaded_model = load_model('ANN_Model.h5')
 def predict_diabetes(data):
     prediction = loaded_model.predict(data)
     prediction = (prediction >= 0.7).astype(int)
+    prediction = prediction[0,0]
     return prediction
 
 def main():
@@ -23,10 +24,11 @@ def main():
     insulin = st.number_input("Insulin Level", min_value=0, value=80)
     bmi = st.number_input("BMI", min_value=0, value=25)
     diabetes_pedigree_function = st.number_input("Diabetes Pedigree Function", min_value=0.0, max_value=2.0, value=0.5, step=0.01)
+    Age_Brack = st.number_input("Skin Thickness", min_value=1, value=8)
 
     if st.button("Predict"):
         # Combine user inputs into a feature array
-        user_input = np.array([pregnancies, glucose, blood_pressure, skin_thickness, insulin, bmi, diabetes_pedigree_function]).reshape(1, -1)
+        user_input = [[pregnancies, glucose, blood_pressure, skin_thickness, insulin, bmi, diabetes_pedigree_function, Age_Brack]]
 
         # Scale the user input
         scaler = MinMaxScaler()
@@ -36,7 +38,7 @@ def main():
         prediction = predict_diabetes(user_input_scaled)
 
         # Display the prediction
-        if prediction > 0.7:
+        if prediction == 0:
             st.write("Prediction: Person has diabetes")
         else:
             st.write("Prediction: Person does not have diabetes")
