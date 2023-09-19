97ebfd4d7dfcc288852bdcfdbde4644b88a461a9
Arya Chakraborty <chakrabortyarya97@gmail.com>
2023-09-19 09:58:11 +0530

    commiting through git

--- app.py
+++ app.py
@@ -13,38 +13,47 @@ def predict_diabetes(data):
     prediction = prediction[0,0]
     return prediction
 
-def main():
-    st.title("Diabetes Prediction Web app")
-    st.sidebar.header("User Input")
+#<======================= Custom CSS & HTML style for the header==========================>
+# Load CSS file
+with open("styles.css") as f:
+    css = f.read()
+    st.markdown(f"<style>{css}</style>", unsafe_allow_html=True)
 
-    pregnancies = st.number_input("Number of Pregnancies", min_value=0, value=0)
-    glucose = st.number_input("Glucose Level", min_value=0, value=100)
-    blood_pressure = st.number_input("Blood Pressure", min_value=0, value=70)
-    skin_thickness = st.number_input("Skin Thickness", min_value=0, value=20)
-    insulin = st.number_input("Insulin Level", min_value=0, value=80)
-    bmi = st.number_input("BMI", min_value=0, value=25)
-    diabetes_pedigree_function = st.number_input("Diabetes Pedigree Function", min_value=0.0, max_value=2.0, value=0.5, step=0.01)
-    Age_Brack = st.number_input("Skin Thickness", min_value=1, value=8)
+# Load HTML file
+with open("Header.html") as f:
+    html = f.read()
+    st.markdown(html, unsafe_allow_html=True)
 
-    if st.button("Predict"):
-        # Combine user inputs into a feature array
-        user_input = [[pregnancies, glucose, blood_pressure, skin_thickness, insulin, bmi, diabetes_pedigree_function, Age_Brack]]
+# <======================================== Sidebar for all details ==============================>
+st.sidebar.header("Diabetes prediction web application")
+with st.sidebar:
+    st.markdown("This Web Application is built by Arya Chakraborty")
+    st.markdown("[My GitHub](www.google.com)")
+    pass
 
-        # Scale the user input
-        scaler = MinMaxScaler()
-        user_input_scaled = scaler.fit_transform(user_input)
+pregnancies = st.number_input("Number of Pregnancies", min_value=0, value=0)
+glucose = st.number_input("Glucose Level", min_value=0, value=100)
+blood_pressure = st.number_input("Blood Pressure", min_value=0, value=70)
+skin_thickness = st.number_input("Skin Thickness", min_value=0, value=20)
+insulin = st.number_input("Insulin Level", min_value=0, value=80)
+bmi = st.number_input("BMI", min_value=0, value=25)
+diabetes_pedigree_function = st.number_input("Diabetes Pedigree Function", min_value=0.0, max_value=2.0, value=0.5, step=0.01)
+Age_Brack = st.number_input("Skin Thickness", min_value=1, value=8)
 
-        # Predict
-        prediction = predict_diabetes(user_input_scaled)
+if st.button("Predict"):
+    # Combine user inputs into a feature array
+    user_input = [[pregnancies, glucose, blood_pressure, skin_thickness, insulin, bmi, diabetes_pedigree_function, Age_Brack]]
 
-        # Display the prediction
-        if prediction == 0:
-            st.write("Prediction: Person has diabetes")
-        else:
-            st.write("Prediction: Person does not have diabetes")
+    # Scale the user input
+    scaler = MinMaxScaler()
+    user_input_scaled = scaler.fit_transform(user_input)
 
+    # Predict
+    prediction = predict_diabetes(user_input_scaled)
 
+    # Display the prediction
+    if prediction == 0:
+        st.write("Prediction: Person has diabetes")
+    else:
+        st.write("Prediction: Person does not have diabetes")
 
-
-if __name__ == "__main__":
-    main()
