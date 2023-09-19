5b779f4697603b69970cc1d69c2c32b2920e367f
Arya Chakraborty <chakrabortyarya97@gmail.com>
2023-09-19 10:53:29 +0530

    adding requirements file

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 # <====================Importing the required libraries==============>
 import streamlit as st
 from tensorflow.keras.models import load_model
-import numpy as np
+from streamlit_lottie import st_lottie
+import requests
 from sklearn.preprocessing import MinMaxScaler
 
+# <====================Streamlit Lottie file==========================>
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+lottie_url_sad = "https://lottie.host/7925a9d3-3ab1-4b22-a6d6-26cf0583cd56/Z8gTeiLv5j.json"
+lottie_url_happy = "https://lottie.host/bd508b2b-31cc-4820-a312-7ba2179d6bae/eeSMpi1CZX.json"
+
+lottie_sad = load_lottieurl(lottie_url_sad)
+lottie_happy = load_lottieurl(lottie_url_happy)
 # <====================Loading the Model========================>
 loaded_model = load_model('ANN_Model.h5')
 
@@ -27,14 +39,15 @@ with open("Header.html") as f:
 # <======================================== Sidebar for all details ==============================>
 st.sidebar.header("Diabetes prediction web application🩺")
 with st.sidebar:
-    st.markdown("This Web Application is built by Arya Chakraborty")
+    st.markdown("**:green[This Web Application is built by Arya Chakraborty]**")
+    st.text("**:blue[Acknowledgement~]**")
     st.markdown("I would like to acknowledge Exposys Data Labs for providing me with the opportunity to work on this project as part of the Data Science internship program. The experience gained during this internship has been invaluable in honing my skills and understanding in the field of data science. I am grateful for the mentorship and support provided by the Exposys Data Labs team throughout the duration of this project.")
     st.markdown("[My GitHub](https://github.com/Arya920)")
     st.markdown("[My LinkedIn](https://www.linkedin.com/in/arya-chakraborty-95a8411b2/)")
     st.markdown("[My Portfolio](https://arya920.github.io/My_Portfolio/)")
 
-    pass
-
+    
+st.markdown('please give the below details carefully.')
 pregnancies = st.number_input("Number of Pregnancies", min_value=0, value=0)
 glucose = st.number_input("Glucose Level", min_value=0, value=100)
 blood_pressure = st.number_input("Blood Pressure", min_value=0, value=70)
@@ -44,7 +57,7 @@ bmi = st.number_input("BMI", min_value=0, value=25)
 diabetes_pedigree_function = st.number_input("Diabetes Pedigree Function", min_value=0.0, max_value=2.0, value=0.5, step=0.01)
 Age_Brack = st.number_input("Skin Thickness", min_value=1, value=8)
 
-if st.button("Predict"):
+if st.button("Check your result"):
     # Combine user inputs into a feature array
     user_input = [[pregnancies, glucose, blood_pressure, skin_thickness, insulin, bmi, diabetes_pedigree_function, Age_Brack]]
 
@@ -57,7 +70,9 @@ if st.button("Predict"):
 
     # Display the prediction
     if prediction == 0:
-        st.write("Prediction: Person has diabetes")
+        st.markdown("**:blue[According to my model you may have Diabetes.]**🥺")
+        st_lottie(lottie_sad, key="sad")
     else:
-        st.write("Prediction: Person does not have diabetes")
+        st.write("**:blue[Mostly you do not have Diabetes. Enjoy]🙌")
+        st_lottie(lottie_happy, key="happy")
 
